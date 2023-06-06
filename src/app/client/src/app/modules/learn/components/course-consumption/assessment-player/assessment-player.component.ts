3fc23dce0c61d51615ecb57a7aab5be59cd19b68
santhosh-tarento <santhosh.kumar@tarento.com>
2023-06-06 01:31:11 -0400

    introducing post api

--- src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
@@ -19,6 +19,7 @@ import { result } from 'lodash';
 import * as uuid from 'uuid';
 const ACCESSEVENT = 'renderer:question:submitscore';
 
+
 @Component({
   selector: 'app-assessment-player',
   templateUrl: './assessment-player.component.html',
@@ -875,7 +876,7 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
       }
       if (this.activeContent.mimeType === this.configService.appConfig.PLAYER_CONFIG.MIME_TYPE.questionset) {
         const serveiceRef = this.userService.loggedIn ? this.playerService : this.publicPlayerService;
-        if(!this.activeContent.evaluable){
+        if(!this.activeContent.serverEvaluable){
         this.publicPlayerService.getQuestionSetHierarchy(id).pipe(
           takeUntil(this.unsubscribe))
           .subscribe((response) => {
@@ -917,10 +918,7 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
             });
             this.publicPlayerService.getQuestionSetHierarchy(id)  
           */
-          this.playerService.getQuestionSetHierarchyByPost(requestBody).subscribe(response => {
-            console.log(response);
-          });
-          this.publicPlayerService.getQuestionSetHierarchy(id).pipe(
+          this.publicPlayerService.getQuestionSetHierarchyByPost(requestBody).pipe(
             takeUntil(this.unsubscribe))
             .subscribe((response) => {
               const objectRollup = this.courseConsumptionService.getContentRollUp(this.courseHierarchy, id);
